dc962a6340b098b2d0d65282d8d005549e9a0193
Matheus Andrade <matheus999299@gmail.com>
2024-09-20 15:08:17 -0300

    adicionando arquivos

---
+++ Gerador_De_Senhas/Program.cs
@@ -0,0 +1,21 @@
+﻿static void Main()
+{
+const string caracteresPermitidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+Console.Write("Insira o comprimento da senha desejada: ");
+int comprimento = int.Parse(Console.ReadLine());
+string senhaGerada = GerarSenhaAleatoria(comprimento, caracteresPermitidos);
+Console.WriteLine($"Senha gerada: {senhaGerada}");
+}
+static string GerarSenhaAleatoria(int comprimento, string caracteres)
+{
+StringBuilder senha = new StringBuilder();
+Random random = new Random();
+
+for (int i = 0; i < comprimento; i++)
+    {
+    int indiceAleatorio = random.Next(caracteres.Length);
+    senha.Append(caracteres[indiceAleatorio]);
+    }
+
+    return senha.ToString();
+}
