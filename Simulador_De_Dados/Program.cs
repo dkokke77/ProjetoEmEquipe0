fb66d83f6715758c31fbe40802bf78b6ac3a451e
Kauan Almeida Moraes <Kauanko123@gmail.com>
2024-09-20 18:38:38 -0300

    adicionando arquivo

---
+++ Simulador_De_Dados/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+string opcao;
+
+do
+    {
+		Console.Clear();
+        int resultado = random.Next(1, 7);
+        Console.WriteLine($"Você lançou o dado e o resultado foi: {resultado}");
+        Console.Write("Deseja lançar o dado novamente? (s/n): ");
+        opcao = Console.ReadLine().ToLower();
+
+    } while (opcao == "s");
+
+        Console.WriteLine("Encerrando o programa. Até a próxima!");
